570d0c56634a87a99e911b681cc4f9d1ec750601
RevenNano <ilix0809@mail.ru>
2019-10-01 22:00:31 +0300

    ru_version

--- src/ComJobLine.java
+++ src/ComJobLine.java
@@ -7,20 +7,20 @@ public class ComJobLine {
         int realLesson = 0;
         int realDay = 0;
 
-        System.out.println("Enter name your of fu**ing kid $> ");
+        System.out.println("Если-бы ввели имя вашего бл****, е***** ребёнка - это ничего бы не изменило -_- $> ");
         String kidName = in.nextLine();
 
-        System.out.println("Enter Real Day, please (Понедельник, вторник, среда, четверг или пятница) $> ");
+        System.out.println("Было-бы круто, если-бы вы ввели сегодняшний день -_- (Понедельник, вторник, среда, четверг или пятница) $> ");
         String realDayName = in.nextLine();
 
-        System.out.println("Enter class of your fu**ing kid $> ");
+        System.out.println("Было-бы круто, если бы вы ввели класс, в котором учится ваш бл****, е***** ребёнок $> ");
         int kidClass = in.nextInt();
 
-        System.out.println("Enter Real Time, please (8.00, 8.30, 8,56 и.т.д) $> ");
+        System.out.println("Было-бы круто, если-бы вы ввели реальное время -_- (8,00; 8,30; 8,56 и.т.д) $> ");
         float realTime = in.nextFloat();
 //---------------------------------------------------------------------
         if(kidClass >= 10 || kidClass < 0){
-            System.out.println(" this school is 9 class, blyt -__- ");
+            System.out.println(" В этой школе всего 9 классов, е**** ты убл***к -_-");
        }
         if(kidClass == 1);
             kidClass--;
@@ -35,26 +35,25 @@ public class ComJobLine {
         if(realTime <= 8.00){
             realLesson = 0;
 
-        } else if (realTime >= 8.00 && realTime <= 8.40){
+        }if (realTime >= 8.00 && realTime <= 8.40){
             realLesson = 1;
 
-        } else if(realTime >= 8.40 && realTime <= 9.00){
+        }if(realTime >= 8.40 && realTime <= 9.00){
             realLesson = 0;
-        } else if(realTime <= 9.00 && realTime <= 9.40){
+        }if(realTime <= 9.00 && realTime <= 9.40){
             realLesson = 2;
-        }
-        if(realTime >= 9.40 && realTime <= 10.00){
+        }if(realTime >= 9.40 && realTime <= 10.00){
             realLesson = 0;
-        } else if (realTime >= 10.00 && realTime <= 10.40){
+        }if (realTime >= 10.00 && realTime <= 10.40){
             realLesson = 3;
-        } else if(realTime >= 10.40 && realTime <= 11.00){
+        }if(realTime >= 10.40 && realTime <= 11.00){
             realLesson = 0;
-        } else if(realTime >= 11.00 && realTime <= 11.40){
+        }if(realTime >= 11.00 && realTime <= 11.40){
             realLesson = 4;
-        } else if(realTime >= 11.40 && realTime <= 12.00)
+        }if(realTime >= 11.40 && realTime <= 12.00)
             realLesson = 0;
 //------------------------------------------------------------- (-_- ) xex
-        System.out.println("После этого п##еца... вот то, что Вы искали: ");
+        System.out.println("Сейчас ваш бл****, е***** ребёнок: ");
         System.out.println(BFTable.schoolClass[kidClass][realDay][realLesson]);
     }
 }
