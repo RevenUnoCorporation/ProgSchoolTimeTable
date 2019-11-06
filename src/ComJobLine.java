89aa434092a49315c4d1d604a3ca24ac04a2f0f0
RevenNano <ilix0809@mail.ru>
2019-11-06 07:42:45 +0300

    Граф. интерфейс работает. Первые попытки прикрутить определение времени

--- src/ComJobLine.java
+++ src/ComJobLine.java
@@ -1,66 +1,45 @@
+/* Основные скрипты */
+
 import java.util.Scanner;
 
 public class ComJobLine {
+    public static String name;
+    public static int kidClass;
+    public static String realDay;
+    public static float realTime;
 
-    public static String lesDir = "/home/revenuno/IdeaProjects/SchoolTimeTable/tables/";
+    public static void setAction() {
 
-    public static void setAction(){
         Scanner in = new Scanner(System.in);
         String realLesson = null;
         String realDay = null;
 
-        System.out.println("Если-бы ввели имя вашего бл****, е***** ребёнка - это ничего бы не изменило -_- $> ");
-        String kidName = in.nextLine();
-
-        System.out.println("Было-бы круто, если-бы вы ввели сегодняшний день -_- (Понедельник, вторник, среда, четверг или пятница) $> ");
-        String realDayName = in.nextLine();
-
-        System.out.println("Было-бы круто, если бы вы ввели класс, в котором учится ваш бл****, е***** ребёнок $> ");
-        int kidClass = in.nextInt();
-
-        System.out.println("Было-бы круто, если-бы вы ввели реальное время -_- (8,00; 8,30; 8,56 и.т.д) $> ");
-        float realTime = in.nextFloat();
-//---------------------------------------------------------------------
-
-        if(kidClass >= 10 || kidClass < 0){
-            System.out.println(" В этой школе всего 9 классов, е**** ты убл***к -_-");
-       }
-
-        switch (realDayName.toUpperCase()){
-            case "ПОНЕДЕЛЬНИК":
-                realDay = "Monday";
-                break;
-            case "ВТОРНИК":
-                realDay = "Thursday";
-                break;
-            default:
-                System.out.println("Нет такого дня недели, е**** ны уёб*к!!");
-        }
-
-//----------------------------------------------------------------   (-_- ) xex
-        if(realTime < 8.00)
-            realLesson = "0";
-        if (realTime >= 8.00 && realTime < 8.40)
-            realLesson = "1";
-        if(realTime >= 8.40 && realTime < 9.00)
-            realLesson = "0";
-        if(realTime >= 9.00 && realTime < 9.40)
-            realLesson = "2";
-        if(realTime >= 9.40 && realTime < 10.00)
-            realLesson = "0";
-        if (realTime >= 10.00 && realTime < 10.40)
-            realLesson = "3";
-        if(realTime >= 10.40 && realTime < 11.00)
-            realLesson = "0";
-        if(realTime >= 11.00 && realTime < 11.40)
-            realLesson = "4";
-        if(realTime >= 11.40 && realTime < 12.00)
-            realLesson = "0";
+//        System.out.println("Было-бы круто, если-бы вы ввели реальное время -_- (8,00; 8,30; 8,56 и.т.д) $> ");
+//        float realTime = in.nextFloat();
+//--------------------------------------------------------------------- хех ( -_-)
+//        if(realTime < 8.00)
+//            realLesson = "0";
+//        if (realTime >= 8.00 && realTime < 8.40)
+//            realLesson = "1";
+//        if(realTime >= 8.40 && realTime < 9.00)
+//            realLesson = "0";
+//        if(realTime >= 9.00 && realTime < 9.40)
+//            realLesson = "2";
+//        if(realTime >= 9.40 && realTime < 10.00)
+//            realLesson = "0";
+//        if (realTime >= 10.00 && realTime < 10.40)
+//            realLesson = "3";
+//        if(realTime >= 10.40 && realTime < 11.00)
+//            realLesson = "0";
+//        if(realTime >= 11.00 && realTime < 11.40)
+//            realLesson = "4";
+//        if(realTime >= 11.40 && realTime < 12.00)
+//            realLesson = "0";
 //------------------------------------------------------------- (-_- ) xex
 
-        lesDir = "/home/revenuno/IdeaProjects/SchoolTimeTable/tables/" + "Class" + kidClass + "/" + realDay + "/" + "less" + realLesson;
-        System.out.println(lesDir);
-        System.out.println("Сейчас ваш бл****, е***** ребёнок на: " + TableJobing.setLesson(lesDir));
+//        lesDir = "/home/revenuno/IdeaProjects/SchoolTimeTable/tables/" + "Class" + kidClass + "/" + realDay + "/" + "less" + realLesson;
+//        JOptionPane.showMessageDialog(null, "Сейчас ваш еб**** бл**** ребёнок на " + TableJobing.setLesson(lesDir), "Героям3Слава", JOptionPane.PLAIN_MESSAGE);
 
     }
+
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,15 +1,35 @@
 /* сдесь будет проиходить весь экшн */
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class MainMenu {
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        String kidName;
-
+        String[] clas = new String[] {"1 class", "2 class"};
+        String[] day = new String[] {"Monday", "Thursday"};
+        ImageIcon img = new ImageIcon("/home/revenuno/IdeaProjects/SchoolTimeTable/img/external");
+//-------------------------------------------------------------------------------
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh");
+//-------------------------------------------------------------------------------
+        JOptionPane.showMessageDialog(null, "vot ono:" + calendar.getTime());
+        ComJobLine.name = JOptionPane.showInputDialog(null, "Как зовут малыша? ;)", "СлаваУкраине!", JOptionPane.QUESTION_MESSAGE);
+        ComJobLine.kidClass = (int)JOptionPane.showInputDialog(null, "В каком он классе? :))", "СлаваУкраине!", JOptionPane.PLAIN_MESSAGE, img, clas, clas[0]);
+        ComJobLine.realDay = (String)JOptionPane.showInputDialog(null, "Какой сегодня день?", "СлаваУкраине!", JOptionPane.PLAIN_MESSAGE, img, day,day[0]);
+//-------------------------------------------------------------------------------
         System.out.println("\t by RavenNano");
         System.out.println("Enter '_help', but program to help you ^-^ ");
         System.out.println(TableJobing.setLesson("/home/revenuno/IdeaProjects/SchoolTimeTable/tables/Class1/Monday/less0"));
+        System.out.println(ComJobLine.kidClass);
 
         System.out.print("$> ");
         String comm = in.nextLine();
